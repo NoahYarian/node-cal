9b5b05c951da6fc066da0302da5fde1305998a64
Noah Yarian <noah.yarian@gmail.com>
2015-07-24 10:01:59 -0500

    tested zeller with more days, added leap year, jan/feb, and saturday modifications for usability

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 //console.log('     July 2015\nSu Mo Tu We Th Fr Sa\n          1  2  3  4\n 5  6  7  8  9 10 11\n12 13 14 15 16 17 18\n19 20 21 22 23 24 25\n26 27 28 29 30 31\n');
-var monthYearStr = require('./lib/monthYearStr');
+var month = require('./lib/month');
+var getDays = require('./lib/getDays');
 
 if (!process.argv[2]) {
-  console.log("no argv[2]");
-  console.log(monthYearStr());
+  console.log(month.getFirstLines());
+  console.log(getDays());
 }
 
 
--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@
 
 var should = require('chai').should();
 var cp = require('child_process');
-var monthYearStr = require('../lib/monthYearStr');
+var month = require('../lib/month');
+var zeller = require('../lib/zeller');
 
 describe('Mocha + Chai', function () {
   it('truthyness', function () {
@@ -14,18 +15,33 @@ describe('Mocha + Chai', function () {
 describe('node-cal', function () {
   it('should return the current month', function () {
     cp.execFile('./app.js', function(err, stdout) {
-      console.log(err);
-      console.log(stdout);
+      console.log("err: ", err);
+      console.log("stdout: ", stdout);
       stdout.should.equal('     July 2015\nSu Mo Tu We Th Fr Sa\n          1  2  3  4\n 5  6  7  8  9 10 11\n12 13 14 15 16 17 18\n19 20 21 22 23 24 25\n26 27 28 29 30 31\n\n');
     });
   });
 });
 
-describe('getMonthYearStr', function() {
+describe('month.getfirstLines', function() {
   it('should return the same as the first two lines of cal', function (done) {
     cp.exec('cal', function(err, stdout) {
-      stdout.slice(0, stdout.indexOf('Sa\n') + 3).should.equal(monthYearStr());
+      stdout.slice(0, stdout.indexOf('Sa\n') + 2).should.equal(month.getFirstLines());
       done();
     });
+  });
+});
+
+describe('zeller', function() {
+  it('should return the correct weekday for 7-23-2015', function () {
+    zeller(7, 23, 20, 15).should.equal(5);
+  });
+  it('should return the correct weekday for 1-3-2015', function () {
+    zeller(1, 3, 20, 15).should.equal(7);
+  });
+  it('should return the correct weekday for 3-3-2014', function () {
+    zeller(3, 3, 20, 14).should.equal(2);
+  });
+  it('should return the correct weekday for 12-16-1900', function () {
+    zeller(12, 16, 19, 0).should.equal(1);
   });
 });
