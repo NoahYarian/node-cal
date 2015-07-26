49ac7fe1e6c55eedc9c9d0513bf33d1500591a68
Noah Yarian <noah.yarian@gmail.com>
2015-07-26 16:03:11 -0500

    integrated getLine1 into month, fixed zeller year subtraction on centuries, added more integration tests, disabled unit getmonth test for now.

--- test/test.js
+++ test/test.js
@@ -15,16 +15,16 @@ describe('Mocha + Chai', function () {
 describe('node-cal', function () {
   it('should return the current month', function () {
     cp.execFile('./app.js', function(err, stdout) {
-      console.log("err: ", err);
-      console.log("stdout: ", stdout);
+      // console.log("err: ", err);
+      // console.log("stdout: ", stdout);
       stdout.should.equal('     July 2015\nSu Mo Tu We Th Fr Sa\n          1  2  3  4\n 5  6  7  8  9 10 11\n12 13 14 15 16 17 18\n19 20 21 22 23 24 25\n26 27 28 29 30 31\n\n');
     });
   });
-  it('should return the same month text as cal for this month', function (done) {
+  it('should match cal when run with no options', function (done) {
     cp.exec('cal', function(err, stdout) {
-      console.log("cal: ", stdout);
+      // console.log("cal: ", stdout);
       cp.execFile('./app.js', function(err2, stdout2) {
-        console.log("./app.js: ", stdout2);
+        // console.log("./app.js: ", stdout2);
         stdout2.should.equal(stdout);
         done();
       });
@@ -38,46 +38,94 @@ describe('node-cal', function () {
       });
     });
   });
-});
-
-describe('getMonth', function() {
-  it('should return the same month text as cal for 1-2015', function (done) {
-    cp.exec('cal 1 2015', function(err, stdout) {
-      stdout.should.equal(getMonth(1, 2015));
-      done();
-    });
-  });
   it('should match cal for a 4-week month like 2-2015', function (done) {
     cp.exec('cal 2 2015', function(err, stdout) {
-      stdout.should.equal(getMonth(2, 2015));
-      done();
+      cp.execFile('./app.js', [2, 2015], function(err2, stdout2) {
+        stdout2.should.equal(stdout);
+        done();
+      });
     });
   });
-  it('should match cal for a 5-week month like 6-1753', function (done) {
-    cp.exec('cal 6 1753', function(err, stdout) {
-      stdout.should.equal(getMonth(6, 1753));
-      done();
+  it('should match cal for a 5-week month like 9-2015', function (done) {
+    cp.exec('cal 9 2015', function(err, stdout) {
+      cp.execFile('./app.js', [9, 2015], function(err2, stdout2) {
+        stdout2.should.equal(stdout);
+        done();
+      });
     });
   });
   it('should match cal for a 6-week month like 8-2015', function (done) {
     cp.exec('cal 8 2015', function(err, stdout) {
-      stdout.should.equal(getMonth(8, 2015));
-      done();
+      cp.execFile('./app.js', [8, 2015], function(err2, stdout2) {
+        stdout2.should.equal(stdout);
+        done();
+      });
     });
   });
-  it('should return the same month text as cal for 3-2014', function (done) {
-    cp.exec('cal 3 2014', function(err, stdout) {
-      stdout.should.equal(getMonth(3, 2014));
-      done();
+  it('should work in a leap year february like 2-2016', function (done) {
+    cp.exec('cal 2 2016', function(err, stdout) {
+      cp.execFile('./app.js', [2, 2016], function(err2, stdout2) {
+        stdout2.should.equal(stdout);
+        done();
+      });
     });
   });
-  it('should return the same month text as cal for 12-1900', function (done) {
-    cp.exec('cal 12 1900', function(err, stdout) {
-      stdout.should.equal(getMonth(12, 1900));
-      done();
+  it('should work in a non-leap year century february like 2-1900', function (done) {
+    cp.exec('cal 2 1900', function(err, stdout) {
+      cp.execFile('./app.js', [2, 1900], function(err2, stdout2) {
+        stdout2.should.equal(stdout);
+        done();
+      });
+    });
+  });
+  it('should work in a leap year century february like 2-2000', function (done) {
+    cp.exec('cal 2 2000', function(err, stdout) {
+      cp.execFile('./app.js', [2, 2000], function(err2, stdout2) {
+        stdout2.should.equal(stdout);
+        done();
+      });
     });
   });
 });
+
+// describe('getMonth', function() {
+//   it('should return the same month text as cal for 1-2015', function (done) {
+//     cp.exec('cal 1 2015', function(err, stdout) {
+//       stdout.should.equal(getMonth(1, 2015));
+//       done();
+//     });
+//   });
+//   it('should match cal for a 4-week month like 2-2015', function (done) {
+//     cp.exec('cal 2 2015', function(err, stdout) {
+//       stdout.should.equal(getMonth(2, 2015));
+//       done();
+//     });
+//   });
+//   it('should match cal for a 5-week month like 6-1753', function (done) {
+//     cp.exec('cal 6 1753', function(err, stdout) {
+//       stdout.should.equal(getMonth(6, 1753));
+//       done();
+//     });
+//   });
+//   it('should match cal for a 6-week month like 8-2015', function (done) {
+//     cp.exec('cal 8 2015', function(err, stdout) {
+//       stdout.should.equal(getMonth(8, 2015));
+//       done();
+//     });
+//   });
+//   it('should return the same month text as cal for 3-2014', function (done) {
+//     cp.exec('cal 3 2014', function(err, stdout) {
+//       stdout.should.equal(getMonth(3, 2014));
+//       done();
+//     });
+//   });
+//   it('should return the same month text as cal for 12-1900', function (done) {
+//     cp.exec('cal 12 1900', function(err, stdout) {
+//       stdout.should.equal(getMonth(12, 1900));
+//       done();
+//     });
+//   });
+// });
 
 describe('zeller', function() {
   it('should return the correct weekday for 7-23-2015', function () {
@@ -91,6 +139,9 @@ describe('zeller', function() {
   });
   it('should return the correct weekday for 12-16-1900', function () {
     zeller(12, 16, 19, 0).should.equal(1);
+  });
+  it('should return the correct weekday for 2-1-1900', function () {
+    zeller(2, 1, 19, 0).should.equal(5);
   });
   it('should return the correct weekday for a leap year - 2-29-2012', function () {
     zeller(2, 29, 20, 12).should.equal(4);
